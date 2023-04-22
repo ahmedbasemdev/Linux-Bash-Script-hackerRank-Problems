1fe0e2babcfe7e575ed4331fa98317796ab07ca5
ahmedbasemdev <ahmedbasem.dev@gmail.com>
2023-04-22 16:22:14 +0200

    first commit

---
+++ 01-A Personalized Echo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# `read` is used to take input from user terminal 
+# and save it in variable called `name`
+read name
+
+# `echo` is used to display the value of agrument
+echo "Welcome $name"
